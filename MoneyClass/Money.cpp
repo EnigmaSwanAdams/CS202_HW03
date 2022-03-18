6125d72801155a22ae638f52ceef3f3b3c5b4829
Enigma Swan Adams <eswanadams@alaska.edu>
2022-03-17 20:38:35 -0800

    added catch and made some changes so everything compiles

--- MoneyClass/Money.cpp
+++ MoneyClass/Money.cpp
@@ -1,8 +1,9 @@
 
 
 
-#include "Money.h"
+#include "Money.hpp"
 #include <iomanip>
+#include <cmath>
 
 // can be declared with either a float representing dollars and cents, 
 // or an int representing cents and an int representing dollars
@@ -11,9 +12,9 @@
 // defualt constructor
 Money::Money() : _cents{ 0 } {}
 // cents and dollars constructor 
-Money::Money(int dollars, int cents) : _cents{ cents + 100 * dollars } {}
+Money::Money(int cents, int dollars) : _cents{ cents + 100 * dollars } {}
 // one double for both cents and dollars
-Money::Money(double dAndc) : _cents{ static_cast<int>(dAndc * 100) } {}
+Money::Money(double dAndc) : _cents{ static_cast<int>(std::round(dAndc * 100))} {}
 
 
 
@@ -21,34 +22,34 @@ Money::Money(double dAndc) : _cents{ static_cast<int>(dAndc * 100) } {}
 
 std::ostream& operator<<(std::ostream& out, const Money& a) {
     if (a < 0) { out << "-"; }
-    out << "$" << std::fixed << std::setprecision(2) <<  a._cents / 100.0;// NOT SURE THIS WORKS !!!
+    out << "$" << std::fixed << std::setprecision(2) <<  abs(a._cents) / 100.0;// NOT SURE THIS WORKS !!!
     return out;
 }
 
 // comparison operators
-// global since I want to compare with numbers (int and float) on either side
-bool operator==(const Money& b, const Money& a){
-    return a._cents == b._cents;
+// local so I think we can't compare Money objects to non Money objects
+bool Money::operator==(const Money& a) const {
+    return _cents == a._cents;
 }
 
-bool operator!=(const Money& b, const Money& a) {
-    return !(a == b);
+bool Money::operator!=( const Money& a) const {
+    return !(a == *this);
 }
 
-bool operator<=(const Money& b, const Money& a) {
-    return (b._cents <= a._cents);
+bool Money::operator<=( const Money& a) const {
+    return (_cents <= a._cents);
 }
 
-bool operator>(const Money& b, const Money& a) {
-    return !(b <= a);
+bool Money::operator>( const Money& a)const {
+    return !(*this <= a);
 }
 
-bool operator>=(const Money& b, const Money& a) {
-    return (b._cents >= a._cents);
+bool Money::operator>=( const Money& a) const {
+    return (_cents >= a._cents);
 }
 
-bool operator<(const Money& b, const Money& a) {
-    return !(b >= a);
+bool Money::operator<( const Money& a) const {
+    return !(*this >= a);
 }
 
 // shorthand mathmatic operators 
@@ -86,6 +87,12 @@ Money operator*(const Money& lhs, const Money& rhs) {
 Money operator+(const Money& lhs, const Money& rhs) {
     auto temp{ lhs };
     temp += rhs;
+    return temp;
+}
+
+Money operator/(const Money& lhs, const Money& rhs) {
+    auto temp{ lhs };
+    temp /= rhs;
     return temp;
 }
 
--- MoneyClass/Money.hpp
+++ MoneyClass/Money.hpp
@@ -8,24 +8,26 @@ public:
     // defualt constructor
     Money();
     // cents and dollars constructor 
-    Money(int, int = 0);
+    Money(int, int=0);
     // one double for both cents and dollars
     Money(double);
 
     // ostream handler for printing 
     friend std::ostream& operator<<(std::ostream& out, const Money& a);
-    friend bool operator==(const Money& b, const Money& a); // it says it can't find a definition, but it compiles 
-    friend bool operator!=(const Money& b, const Money& a); // presumably this one too
-    friend bool operator<=(const Money& b, const Money& a);
-    friend bool operator>(const Money& b, const Money& a);
-    friend bool operator>=(const Money& b, const Money& a);
-    friend bool operator<(const Money& b, const Money& a);
+    bool operator==(const Money& a) const;
+    bool operator!=( const Money& a) const; 
+    bool operator<=( const Money& a) const;
+    bool operator>(const Money& a) const;
+    bool operator>=(const Money& a) const;
+    bool operator<(const Money& a) const;
     Money operator+=(const Money& rhs);
     Money operator*=(const Money& rhs);
     friend Money operator*(const Money& lhs, const Money& rhs);
     friend Money operator+(const Money& lhs, const Money& rhs);
     Money operator-=(const Money& rhs);
     Money operator/=(const Money& rhs);
+    friend Money operator/(const Money& lhs, const Money& rhs);
+    friend Money operator-(const Money& lhs, const Money& rhs);
 
 
 private:
